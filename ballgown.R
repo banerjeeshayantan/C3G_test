0ec8c1806f669a73dbe2159e77a14bbd9144132c
Shayantan Banerjee <banerjeeshayantan@users.noreply.github.com>
2020-02-25 12:10:11 +0530

    Update ballgown.R

--- ballgown.R
+++ ballgown.R
@@ -1,4 +1,7 @@
 library(ballgown)
+#The samples are stored in two seperate folders sample_brain and sample_liver for ballgown to access
 bg = ballgown(dataDir="/data/shayantan/sample_ballgoown//", samplePattern='sample', meas='all')
+#storing the fpkms in transcript_fpkm variable
 transcript_fpkm = texpr(bg, 'FPKM')
-write.table(transcript_fpkm,"FPKM_1000.csv",sep="\t",row.names = FALSE,col.names = TRUE,quote = FALSE)
+#writing the results of the first 1000 lines of the transcript_fpkm
+write.table(transcript_fpkm[1:1000],"FPKM_1000.csv",sep="\t",row.names = FALSE,col.names = TRUE,quote = FALSE)
